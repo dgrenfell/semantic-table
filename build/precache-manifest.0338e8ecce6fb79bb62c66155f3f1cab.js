6dd4ad975e8cca106d1b61d311e1dbf8336d82eb
Daniel Grenfell <daniel.grenfell1@gmail.com>
2022-04-30 20:05:35 +0100

    updated README.md

---
+++ build/precache-manifest.0338e8ecce6fb79bb62c66155f3f1cab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd58c02c983cf1484426",
+    "url": "/static/css/main.3266eb91.chunk.css"
+  },
+  {
+    "revision": "cd58c02c983cf1484426",
+    "url": "/static/js/main.cd58c02c.chunk.js"
+  },
+  {
+    "revision": "08c9a6d907634c974c93",
+    "url": "/static/js/1.08c9a6d9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a753e46f6c3282614c40299f78bb0e41",
+    "url": "/index.html"
+  }
+];
